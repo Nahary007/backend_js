a29ba83b20db1e53d5180248b41282b2cb393e4a
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-09-11 10:08:40 +0300

    feat/create entity

---
+++ backend/src/modules/vehicle/entities/vehicle.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Booking } from "src/modules/booking/entities/booking.entity";
+
+@Entity()
+export class Vehicle {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    plateNumber : string;
+
+    @Column()
+    ownerName: string;
+
+    @OneToMany(() => Booking, (booking) => booking.parking)
+    bookings: Booking[];
+}
