d6cb68871ee351ac443ab28fdbd9c330e44f222f
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-09-11 12:38:15 +0300

    fix/fixing route and controller

--- backend/src/modules/parking/parking.module.ts
+++ backend/src/modules/parking/parking.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Parking } from "./entities/parking.entity";
+import { ParkingController } from "./controllers/parking.controller";
+import { ParkingService } from "./services/parking.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Parking])],
-    // controllers: [ParkingController],
-    // providers: [ParkingService],
-    // exports: [ParkingService],
+    controllers: [ParkingController],
+    providers: [ParkingService],
+    exports: [ParkingService],
 })
 
 export class ParkingModule{}
