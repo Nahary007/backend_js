6d5cee8beb0f8483e63ac3d422e157947d87ae07
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-09-11 12:15:42 +0300

    feat/add controller and services

---
+++ backend/src/modules/parking/controllers/parking.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Param, Delete, Put } from "@nestjs/common";
+import { ParkingService } from "../services/parking.service";
+import { CreateParkingDto } from "../dto/create-parking.dto";
+import { UpdateParkingDto } from "../dto/delete-parking.dto";
+
+@Controller('parkings')
+export class ParkingController {
+    constructor(private readonly parkingService: ParkingService) {}
+
+    @Post()
+    create(@Body() dto: CreateParkingDto) {
+        return this.parkingService.create(dto);
+    }
+
+    @Get()
+    findAll() {
+        return this.parkingService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.parkingService.findOne(+id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() dto: UpdateParkingDto) {
+        return this.parkingService.update(+id, dto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.parkingService.remove(+id);
+    }
+}
---
+++ backend/src/modules/parking/services/parking.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Parking } from "../entities/parking.entity";
+import { CreateParkingDto } from "../dto/create-parking.dto";
+import { UpdateParkingDto } from "../dto/delete-parking.dto";
+
+@Injectable()
+export class ParkingService {
+    constructor(
+        @InjectRepository(Parking)
+        private repo: Repository<Parking>,
+    ) {}
+
+    create(dto: CreateParkingDto) {
+        const parking = this.repo.create(dto);
+        return this.repo.save(parking);
+    }
+
+    findAll() {
+        return this.repo.find();
+    }
+
+    findOne(id: number) {
+        return this.repo.findOne({ where: { id } });
+    }
+
+    update(id: number, dto: UpdateParkingDto) {
+        return this.repo.update(id, dto);
+    }
+
+    remove(id: number) {
+        return this.repo.delete(id);
+    }
+}
