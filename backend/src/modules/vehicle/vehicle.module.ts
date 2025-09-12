5edaf1f12f34a4eef19e20b1cccf7629c40229eb
Masy1608@gmail.com <maholyrkt@gmail.com>
2025-09-12 15:03:31 +0300

    Add vehicule backend

--- backend/src/modules/vehicle/vehicle.module.ts
+++ backend/src/modules/vehicle/vehicle.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Vehicle } from "./entities/vehicle.entity";
+import { VehicleService } from "./services/vehicle.service";
+import { VehicleController } from "./controllers/vehicle.controller";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Vehicle])],
-    // controllers: [ParkingController],
-    // providers: [ParkingService],
-    // exports: [ParkingService],
+  imports: [TypeOrmModule.forFeature([Vehicle])],
+  controllers: [VehicleController],
+  providers: [VehicleService],
 })
-
-export class VehicleModule{}
+export class VehicleModule {}
