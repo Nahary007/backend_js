820e0b8ab6fb20e397ea7bfe0ee54d52693ceb83
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-09-17 23:11:39 +0300

    fix/resolve conflict #3

--- backend/src/modules/vehicle/dto/read-vehicle.dto.ts
+++ backend/src/modules/vehicle/dto/read-vehicle.dto.ts
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-import { Exclude } from "class-transformer";
-
-=======
->>>>>>> resolve_conflict
 export class ReadVehicleDto {
   id: number;
   plateNumber: string;
   ownerName: string;
-<<<<<<< HEAD
-}
-=======
   parkingId: number;
   duration: number;
   startTime: string;
 }
->>>>>>> resolve_conflict
